0962fcb2b352ed659a14bf75e6e084ac0b93d32c
shankariya, saurabh <sshankariya@gmail.com>
2016-06-30 22:52:46 +0530

    initial commit

---
+++ app.ts
@@ -0,0 +1,7 @@
+/// <reference path="./typings/angularjs/angular.d.ts" />
+
+module fullNameApp {
+    'use strict';
+    
+    var webApp = angular.module('nameApp', []);
+}
---
+++ appConfig/app.route.ts
@@ -0,0 +1,30 @@
+/// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angularjs/angular-route.d.ts" />
+
+module fullnameApp {
+    "use strict";
+
+    class AppRoutes{
+        static $inject = ["$routeProvider"];
+        static configRoutes($routeProvider: ng.route.IRouteProvider){
+            $routeProvider
+                .when("/home", {
+                    templateUrl: '../views/home.html',
+                    controller: 'homeCtrl',
+                    controllerAs: 'vm'   
+                })
+                .when("/display", {
+                    templateUrl: '../views/display.html',
+                    controller: 'dispalyCtrl',
+                    controllerAs: 'vm'                       
+                })
+               .otherwise({
+                    redirectTo: '/home'
+                });
+        }
+    }
+    
+    angular
+    .module('app.routes', ['ngRoute'])
+    .config(AppRoutes.configRoutes);
+}
